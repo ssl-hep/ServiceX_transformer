1d946588027748221d619102ee3f53d41ffa8bf0
Marc Weinberg <mweinberg2718@gmail.com>
2019-04-23 16:00:01 -0500

    Added timing checks to test performance. Tree only loads specifically requested branch (for now)

--- printXaodBranches.C
+++ printXaodBranches.C
@@ -1,8 +1,31 @@
-void printXaodBranches() {
+int printXaodBranches(TString inFile, TString inBranch) {
+    TStopwatch sw = TStopwatch();
+    sw.Start();
+
     xAOD::Init();
-    TFile* f = TFile::Open("/xaodFiles/AOD.11182705._000001.pool.root.1");
+    
+    TFile* f = TFile::Open(inFile);
     TTree* t = xAOD::MakeTransientTree(f);
     gSystem->RedirectOutput("temp.txt", "w");
-    t->Show();
+    // t->Show();
+    t->Print();
     gSystem->RedirectOutput(0);
+
+    t->SetBranchStatus("*", 0);
+    t->SetBranchStatus(inBranch, 1);
+    std::cout << "Number of input events: " << t->GetEntries() << std::endl;
+    for(int jEntry = 0; jEntry < t->GetEntries(); jEntry++) {
+        t->GetEntry(jEntry);
+
+        if(jEntry % 1000 == 0) {
+            std::cout << "Processing entry " << jEntry << std::endl;
+        }
+    }
+    
+    xAOD::ClearTransientTrees();
+
+    sw.Stop();
+    std::cout << "Real time: " << sw.RealTime() / 60.0 << " minutes" << std::endl;
+    std::cout << "CPU time:  " << sw.CpuTime() / 60.0 << " minutes" << std::endl;
+    return 0;
 }
