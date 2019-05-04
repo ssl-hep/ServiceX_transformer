a8e546730c487cc5d7c062bfe7739d5940f73555
Marc Weinberg <mweinberg2718@gmail.com>
2019-05-04 01:50:07 -0500

    Now accepts arbitrary collection attributes

--- xaod_branches.py
+++ xaod_branches.py
@@ -5,23 +5,28 @@ import ROOT
 import numpy as np
 ROOT.gROOT.Macro('$ROOTCOREDIR/scripts/load_packages.C')
 
-# Initialize the xAOD infrastructure:
-if not ROOT.xAOD.Init().isSuccess():
-    print("Failed xAOD.Init()")
 
 
-
-def print_branches(file_name):
+def print_branches(file_name, branch_name):
     file_in = ROOT.TFile.Open(file_name)
     tree_in = ROOT.xAOD.MakeTransientTree(file_in)
 
     ROOT.gSystem.RedirectOutput('temp.txt', 'w')
     tree_in.Print()
+
+    # To print the attributes of a particle collection
+    ROOT.gSystem.RedirectOutput('xaod_branch_attributes.txt', 'w')
+    tree_in.GetEntry(0)
+    particles = getattr(tree_in, branch_name)
+    if particles.size() >= 1:
+        for method_name in dir(particles.at(0)):
+            print(method_name)
+    
     ROOT.gROOT.ProcessLine("gSystem->RedirectOutput(0);")
 
 
 
-def write_branches_to_ntuple(file_name, branch_name):
+def write_branches_to_ntuple(file_name, branch_name, attr_name_list):
     sw = ROOT.TStopwatch()
     sw.Start()
     
@@ -35,20 +40,18 @@ def write_branches_to_ntuple(file_name, branch_name):
     n_entries = tree_in.GetEntries()
     print("Total entries: " + str(n_entries))
 
-    file_out = ROOT.TFile('flat_ntuple.root', 'recreate')
-    tree_out = ROOT.TTree('flat_ntuple', "Flat ntuple")
+    file_out = ROOT.TFile('flat_file.root', 'recreate')
+    tree_out = ROOT.TTree('flat_tree', '')
 
     n_particles = np.zeros(1, dtype=int)
-    particle_pt = ROOT.std.vector('float')()
-    particle_eta = ROOT.std.vector('float')()
-    particle_phi = ROOT.std.vector('float')()
-    particle_e = ROOT.std.vector('float')()
+    particle_attr = {}
+    for attr_name in attr_name_list:
+        particle_attr[attr_name] = ROOT.std.vector('float')()
 
     b_n_particles = tree_out.Branch('n_particles', n_particles, 'n_particles/I')
-    b_particle_pt = tree_out.Branch('particle_pt', particle_pt)
-    b_particle_eta = tree_out.Branch('particle_eta', particle_eta)
-    b_particle_phi = tree_out.Branch('particle_phi', particle_phi)
-    b_particle_e = tree_out.Branch('particle_e', particle_e)
+    b_particle_attr = {}
+    for attr_name in attr_name_list:
+        b_particle_attr[attr_name] = tree_out.Branch('particle_' + attr_name, particle_attr[attr_name])
 
     for j_entry in xrange(n_entries):
         tree_in.GetEntry(j_entry)
@@ -57,16 +60,17 @@ def write_branches_to_ntuple(file_name, branch_name):
                   + ", event #" + str(tree_in.EventInfo.eventNumber())
                   + " (" + str(round(100.0 * j_entry / n_entries, 2)) + "%)")
 
-        exec("particles = tree_in." + branch_name)
-
+        particles = getattr(tree_in, branch_name)
         n_particles[0] = particles.size()
-        particle_pt.clear()
+        for attr_name in attr_name_list:
+            particle_attr[attr_name].clear()
         for i in xrange(particles.size()):
             particle = particles.at(i)
-            particle_pt.push_back(particle.trackParticle().pt())
-            particle_eta.push_back(particle.trackParticle().eta())
-            particle_phi.push_back(particle.trackParticle().phi())
-            particle_e.push_back(particle.trackParticle().e())
+
+            # Maybe enforce branch type with:
+            # if exec("type(particle." + attr_name + "())") == float:
+            for attr_name in attr_name_list:
+                exec("particle_attr[attr_name].push_back(particle." + attr_name + "())")
         
         tree_out.Fill()
 
