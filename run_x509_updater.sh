de503e8d70b9b66c2485714ebeb37a192ee9a4ae
Ilija Vukotic <ivukotic@cern.ch>
2019-05-06 17:06:09 -0500

    small dev

--- run_x509_updater.sh
+++ run_x509_updater.sh
@@ -2,7 +2,7 @@
 
 # Need to install this - it will create and populate directory /etc/grid-security/certificates
 mkdir /etc/grid-security/vomsdir 
-yum install osg-ca-certs voms-clients wlcg-voms-atlas fetch-crl -y
+sudo yum install osg-ca-certs voms-clients wlcg-voms-atlas fetch-crl -y
 
 CERTPATH=/etc/grid-certs
 
@@ -10,7 +10,7 @@ while true; do
 
   # update proxy
   voms-proxy-init -key $CERTPATH/userkey.pem -cert $CERTPATH/usercert.pem --voms=atlas
-  mv /tmp/x509up_u0 /etc/grid-security/x509up
+  # mv /tmp/x509up_u0 /etc/grid-security/x509up
   # chown ivukotic /etc/grid-security/x509up
 
   sleep 21600
--- transform_starter.py
+++ transform_starter.py
@@ -15,8 +15,22 @@ time.sleep(60)
 es = Elasticsearch([conf['ES_HOST']], timeout=60)
 
 while True:
-    res = es.search(index="servicex", body={"query": {"match": {"status": "Prescreened"}}})
+    res = es.search(index="servicex", body={"size": 1, "query": {"match": {"status": "Prescreened"}}})
     if res['hits']['total']:
         print("Got %d Hits:" % res['hits']['total'])
+    else:
+        time.sleep(10)
+        continue
 
-    time.sleep(10)
+    hit = res['hits']['hits'][0]
+    print(hit)
+
+    print('lookin up the first file.')
+    ff = es.search(index="servicex_paths", body={"size": 1, "query": {"match": {"req_id": hit['_id']}}})
+    if res['hits']['total']:
+        print("Got %d files:" % res['hits']['total'])
+    else:
+        print("Serious issue: Could not get the path to a file.")
+        continue
+    fh = ff['hits']['hits'][0]
+    print(fh)
