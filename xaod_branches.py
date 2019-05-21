840a2d8f7f615a537fe12e403e051e42efcb8832
Marc Weinberg <mweinberg2718@gmail.com>
2019-05-21 13:51:50 -0500

    Added function to write branches to Arrow buffers

--- xaod_branches.py
+++ xaod_branches.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-# Set up ROOT and RootCore:
+# Set up ROOT, uproot, and RootCore:
 import ROOT
 import numpy as np
+import awkward
 ROOT.gROOT.Macro('$ROOTCOREDIR/scripts/load_packages.C')
 
 
@@ -26,16 +27,24 @@ def print_branches(file_name, branch_name):
 
 
 
-def write_branches_to_ntuple(file_name, branch_name, attr_name_list):
+def write_branches_to_ntuple(file_name, attr_name_list):
     sw = ROOT.TStopwatch()
     sw.Start()
     
     file_in = ROOT.TFile.Open(file_name)
     tree_in = ROOT.xAOD.MakeTransientTree(file_in)
 
+    branches = {}
+    for attr_name in attr_name_list:
+        if not attr_name.split('.')[0] in branches:
+            branches[attr_name.split('.')[0]] = [attr_name.split('.')[1]]
+        else:
+            branches[attr_name.split('.')[0]].append(attr_name.split('.')[1])
+
     tree_in.SetBranchStatus('*', 0)
     tree_in.SetBranchStatus('EventInfo', 1)
-    tree_in.SetBranchStatus(branch_name, 1)
+    for branch_name in branches:
+        tree_in.SetBranchStatus(branch_name, 1)
     
     n_entries = tree_in.GetEntries()
     print("Total entries: " + str(n_entries))
@@ -43,15 +52,24 @@ def write_branches_to_ntuple(file_name, branch_name, attr_name_list):
     file_out = ROOT.TFile('flat_file.root', 'recreate')
     tree_out = ROOT.TTree('flat_tree', '')
 
-    n_particles = np.zeros(1, dtype=int)
     particle_attr = {}
+    for branch_name in branches:
+        particle_attr[branch_name] = np.zeros(1, dtype=int)
     for attr_name in attr_name_list:
         particle_attr[attr_name] = ROOT.std.vector('float')()
 
-    b_n_particles = tree_out.Branch('n_particles', n_particles, 'n_particles/I')
     b_particle_attr = {}
+    for branch_name in branches:
+        b_particle_attr[branch_name] = tree_out.Branch(
+            'n_' + branch_name,
+            particle_attr[branch_name],
+            'n_' + branch_name + '/I'
+            )
     for attr_name in attr_name_list:
-        b_particle_attr[attr_name] = tree_out.Branch('particle_' + attr_name, particle_attr[attr_name])
+        b_particle_attr[attr_name] = tree_out.Branch(
+            attr_name.split('.')[0] + '_' + attr_name.split('.')[1].strip('()'),
+            particle_attr[attr_name]
+            )
 
     for j_entry in xrange(n_entries):
         tree_in.GetEntry(j_entry)
@@ -60,17 +78,16 @@ def write_branches_to_ntuple(file_name, branch_name, attr_name_list):
                   + ", event #" + str(tree_in.EventInfo.eventNumber())
                   + " (" + str(round(100.0 * j_entry / n_entries, 2)) + "%)")
 
-        particles = getattr(tree_in, branch_name)
-        n_particles[0] = particles.size()
-        for attr_name in attr_name_list:
-            particle_attr[attr_name].clear()
-        for i in xrange(particles.size()):
-            particle = particles.at(i)
-
-            # Maybe enforce branch type with:
-            # if exec("type(particle." + attr_name + "())") == float:
-            for attr_name in attr_name_list:
-                exec("particle_attr[attr_name].push_back(particle." + attr_name + "())")
+        particles = {}
+        for branch_name in branches:
+            particles[branch_name] = getattr(tree_in, branch_name)
+            particle_attr[branch_name][0] = particles[branch_name].size()
+            for a_name in branches[branch_name]:
+                attr_name = branch_name + '.' + a_name
+                particle_attr[attr_name].clear()
+                for i in xrange(particle_attr[branch_name]):
+                    particle = particles[branch_name].at(i)
+                    exec('particle_attr[attr_name].push_back(particle.' + a_name + ')')
         
         tree_out.Fill()
 
@@ -81,3 +98,58 @@ def write_branches_to_ntuple(file_name, branch_name, attr_name_list):
     sw.Stop()
     print("Real time: " + str(round(sw.RealTime() / 60.0, 2)) + " minutes")
     print("CPU time:  " + str(round(sw.CpuTime() / 60.0, 2)) + " minutes")
+
+
+
+def write_branches_to_arrow(file_name, attr_name_list):
+    sw = ROOT.TStopwatch()
+    sw.Start()
+    
+    file_in = ROOT.TFile.Open(file_name)
+    tree_in = ROOT.xAOD.MakeTransientTree(file_in)
+
+    branches = {}
+    for attr_name in attr_name_list:
+        if not attr_name.split('.')[0] in branches:
+            branches[attr_name.split('.')[0]] = [attr_name.split('.')[1]]
+        else:
+            branches[attr_name.split('.')[0]].append(attr_name.split('.')[1])
+    
+    tree_in.SetBranchStatus('*', 0)
+    tree_in.SetBranchStatus('EventInfo', 1)
+    for branch_name in branches:
+        tree_in.SetBranchStatus(branch_name, 1)
+
+    object_array = awkward.fromiter(make_event_table(tree_in, branches))
+    object_table = awkward.Table(pt=object_array['Electrons.pt()'], eta=object_array['Electrons.eta()'])
+
+    ROOT.xAOD.ClearTransientTrees()
+
+    sw.Stop()
+    print("Real time: " + str(round(sw.RealTime() / 60.0, 2)) + " minutes")
+    print("CPU time:  " + str(round(sw.CpuTime() / 60.0, 2)) + " minutes")
+
+
+
+def make_event_table(tree, branches):
+    n_entries = tree.GetEntries()
+    print("Total entries: " + str(n_entries))
+
+    for j_entry, event in enumerate(tree):
+        if j_entry % 1000 == 0:
+            print("Processing run #" + str(event.EventInfo.runNumber())
+                  + ", event #" + str(event.EventInfo.eventNumber())
+                  + " (" + str(round(100.0 * j_entry / n_entries, 2)) + "%)")
+        
+        full_event = []
+        for branch_name in branches:
+            for particle in getattr(event, branch_name):
+                particle_attr = {}
+                for a_name in branches[branch_name]:
+                    attr_name = branch_name + '.' + a_name
+                    exec('particle_attr[attr_name] = particle.' + a_name)
+                full_event.append(particle_attr)
+
+        yield full_event
+
+        # if j_entry == 5: break
