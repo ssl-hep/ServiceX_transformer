74ce31c3be4d6b54867375dc34edb1daaf1a0e5b
Ilija Vukotic <ivukotic@cern.ch>
2019-07-24 00:22:38 -0500

    processing file by file until memory leak fixed.

--- xaod_branches.py
+++ xaod_branches.py
@@ -4,6 +4,7 @@ from __future__ import division
 # Set up ROOT, uproot, and RootCore:
 import math
 import os
+import sys
 import ROOT
 import numpy as np
 import pyarrow as pa
@@ -142,7 +143,7 @@ def write_branches_to_ntuple(file_name, attr_name_list):
 
 
 def write_branches_to_arrow():
-    
+
     rpath_output = requests.get('https://servicex.slateci.net/dpath/transform', verify=False)
 
     if rpath_output.text == 'false':
@@ -194,13 +195,13 @@ def write_branches_to_arrow():
             branch_name = attr_name.split('.')[0].strip(' ')
             a_name = attr_name.split('.')[1]
             table_def = (table_def + branch_name + '_' + a_name.strip('()')
-                            + "=object_array['" + branch_name + "']['"
-                            + a_name + "'], ")
+                         + "=object_array['" + branch_name + "']['"
+                         + a_name + "'], ")
         last_branch_name = attr_name_list[-1].split('.')[0].strip(' ')
         last_a_name = attr_name_list[-1].split('.')[1]
         table_def = (table_def + last_branch_name + '_' + last_a_name.strip('()')
-                        + "=object_array['" + last_branch_name + "']['"
-                        + last_a_name + "'])")
+                     + "=object_array['" + last_branch_name + "']['"
+                     + last_a_name + "'])")
         object_table = eval(table_def)
 
         pa_table = awkward.toarrow(object_table)
@@ -221,9 +222,9 @@ def write_branches_to_arrow():
                     print("Batch number " + str(batch_number) + ", " + str(batch.num_rows) + " events published to " + _request_id)
                     batch_number += 1
                     requests.put('https://servicex.slateci.net/drequest/events_served/' +
-                                    _request_id + '/' + str(batch.num_rows), verify=False)
+                                 _request_id + '/' + str(batch.num_rows), verify=False)
                     requests.put('https://servicex.slateci.net/dpath/events_served/' +
-                                    _id + '/' + str(batch.num_rows), verify=False)
+                                 _id + '/' + str(batch.num_rows), verify=False)
                     break
                 else:
                     print("not published. Waiting 10 seconds before retry.")
@@ -235,6 +236,7 @@ def write_branches_to_arrow():
     sw.Stop()
     print("Real time: " + str(round(sw.RealTime() / 60.0, 2)) + " minutes")
     print("CPU time:  " + str(round(sw.CpuTime() / 60.0, 2)) + " minutes")
+    sys.exit(0)
 
 
 if __name__ == "__main__":
