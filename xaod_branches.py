a1fc6688767995a978b32eb0045486820f28bed7
Ben Galewsky <bengal1@illinois.ed>
2019-08-14 10:48:08 -0500

    Remove unused code

--- xaod_branches.py
+++ xaod_branches.py
@@ -99,6 +99,10 @@ parser.add_argument("--max-message-size", dest='max_message_size',
                     action='store', default=default_max_message_size,
                     help='Max message size in megabytes')
 
+parser.add_argument('--rabbit-uri', dest="rabbit_uri", action='store',
+                    default='host.docker.internal')
+
+
 ROOT.gROOT.Macro('$ROOTCOREDIR/scripts/load_packages.C')
 
 
@@ -133,97 +137,6 @@ def make_event_table(tree, branches, f_evt, l_evt):
         yield full_event
 
         # if j_entry == 6000: break
-
-
-def print_branches(file_name, branch_name):
-    file_in = ROOT.TFile.Open(file_name)
-    tree_in = ROOT.xAOD.MakeTransientTree(file_in)
-
-    ROOT.gSystem.RedirectOutput('temp.txt', 'w')
-    tree_in.Print()
-
-    # To print the attributes of a particle collection
-    ROOT.gSystem.RedirectOutput('xaod_branch_attributes.txt', 'w')
-    tree_in.GetEntry(0)
-    particles = getattr(tree_in, branch_name)
-    if particles.size() >= 1:
-        for method_name in dir(particles.at(0)):
-            print(method_name)
-
-    ROOT.gROOT.ProcessLine("gSystem->RedirectOutput(0);")
-
-
-def write_branches_to_ntuple(file_name, attr_name_list):
-    sw = ROOT.TStopwatch()
-    sw.Start()
-
-    file_in = ROOT.TFile.Open(file_name)
-    tree_in = ROOT.xAOD.MakeTransientTree(file_in)
-
-    branches = {}
-    for attr_name in attr_name_list:
-        if not attr_name.split('.')[0].strip(' ') in branches:
-            branches[attr_name.split('.')[0].strip(' ')] = [attr_name.split('.')[1]]
-        else:
-            branches[attr_name.split('.')[0].strip(' ')].append(attr_name.split('.')[1])
-
-    tree_in.SetBranchStatus('*', 0)
-    tree_in.SetBranchStatus('EventInfo', 1)
-    for branch_name in branches:
-        tree_in.SetBranchStatus(branch_name, 1)
-
-    n_entries = tree_in.GetEntries()
-    print("Total entries: " + str(n_entries))
-
-    file_out = ROOT.TFile('flat_file.root', 'recreate')
-    tree_out = ROOT.TTree('flat_tree', '')
-
-    particle_attr = {}
-    for branch_name in branches:
-        particle_attr[branch_name] = np.zeros(1, dtype=int)
-    for attr_name in attr_name_list:
-        particle_attr[attr_name] = ROOT.std.vector('float')()
-
-    b_particle_attr = {}
-    for branch_name in branches:
-        b_particle_attr[branch_name] = tree_out.Branch(
-            'n_' + branch_name,
-            particle_attr[branch_name],
-            'n_' + branch_name + '/I'
-        )
-    for attr_name in attr_name_list:
-        b_particle_attr[attr_name] = tree_out.Branch(
-            attr_name.split('.')[0].strip(' ') + '_' + attr_name.split('.')[1].strip('()'),
-            particle_attr[attr_name]
-        )
-
-    for j_entry in xrange(n_entries):
-        tree_in.GetEntry(j_entry)
-        if j_entry % 1000 == 0:
-            print("Processing run #" + str(tree_in.EventInfo.runNumber())
-                  + ", event #" + str(tree_in.EventInfo.eventNumber())
-                  + " (" + str(round(100.0 * j_entry / n_entries, 2)) + "%)")
-
-        particles = {}
-        for branch_name in branches:
-            particles[branch_name] = getattr(tree_in, branch_name)
-            particle_attr[branch_name][0] = particles[branch_name].size()
-            for a_name in branches[branch_name]:
-                attr_name = branch_name + '.' + a_name
-                particle_attr[attr_name].clear()
-                for i in xrange(particle_attr[branch_name]):
-                    particle = particles[branch_name].at(i)
-                    exec('particle_attr[attr_name].push_back(particle.' + a_name + ')')
-
-        tree_out.Fill()
-
-    file_out.Write()
-    file_out.Close()
-    ROOT.xAOD.ClearTransientTrees()
-
-    sw.Stop()
-    print("Real time: " + str(round(sw.RealTime() / 60.0, 2)) + " minutes")
-    print("CPU time:  " + str(round(sw.CpuTime() / 60.0, 2)) + " minutes")
 
 
 def poll_for_root_files(servicex, messaging, chunk_size, wait_for_consumer, event_limit=None):
