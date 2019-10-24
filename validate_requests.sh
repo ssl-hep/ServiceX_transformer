f3a47f44bd52db17602df807d42f1daf229ecd58
Ben Galewsky <bengal1@illinois.ed>
2019-10-24 12:02:31 -0500

    Remove unused files

--- validate_requests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set up the environment
-source /home/atlas/release_setup.sh
-# echo '{gROOT->Macro("$ROOTCOREDIR/scripts/load_packages.C");}' > rootlogon.C
-
-export PYTHONWARNINGS="ignore:Unverified HTTPS request"
-python validate_requests.py
-
