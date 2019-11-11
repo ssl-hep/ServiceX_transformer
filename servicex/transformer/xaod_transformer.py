f5dd80745b34bb4217f3f54499fdd766f2e5344f
Marc Weinberg <mweinberg2718@gmail.com>
2019-11-11 01:33:41 -0600

    More changes

--- servicex/transformer/xaod_transformer.py
+++ servicex/transformer/xaod_transformer.py
@@ -54,20 +54,17 @@ class XAODTransformer:
                     yield results
 
         for events in group(self.event_iterator.iterate(event_limit), chunk_size):
-            print("Hello world 1")
             object_array = awkward.fromiter(events)
-            print("Hello world 2")
             attr_dict = {}
-            print("Hello world 3")
             for attr_name in self.event_iterator.attr_name_list:
+                print("Hello world 1")
                 branch_name = attr_name.split('.')[0].strip(' ')
+                print("Hello world 2")
                 a_name = attr_name.split('.')[1]
-
+                print("Hello world 3")
                 attr_dict[branch_name + '_' + a_name.strip('()')] = \
                     object_array[branch_name][a_name]
+                print("Hello world 4")
 
-            print("Hello world 4")
             object_table = awkward.Table(**attr_dict)
-            print("Hello world 5")
             yield awkward.toarrow(object_table)
-            print("Hello world 6")
