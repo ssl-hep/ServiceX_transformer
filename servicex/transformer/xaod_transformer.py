ce39c21c5d691cfcf9880a42a33de510d0abdf2d
Marc Weinberg <mweinberg2718@gmail.com>
2019-11-11 03:18:42 -0600

    Reorder print statements

--- servicex/transformer/xaod_transformer.py
+++ servicex/transformer/xaod_transformer.py
@@ -59,9 +59,9 @@ class XAODTransformer:
             for attr_name in self.event_iterator.attr_name_list:
                 branch_name = str(attr_name.split('.')[0].strip(' '))
                 a_name = str(attr_name.split('.')[1])
-                print(branch_name + '_' + a_name.strip('()'))
                 print(branch_name)
                 print(a_name)
+                print(branch_name + '_' + a_name.strip('()'))
                 attr_dict[branch_name + '_' + a_name.strip('()')] = \
                     object_array[branch_name][a_name]
 
