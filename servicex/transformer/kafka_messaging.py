569c4655d45d415916a841b6ee28665081bd7edb
Ben Galewsky <ben@peartreestudio.net>
2020-02-03 16:08:35 -0600

    Make python3 compatible

--- servicex/transformer/kafka_messaging.py
+++ servicex/transformer/kafka_messaging.py
@@ -26,7 +26,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 import sys
-from messaging import Messaging
+from .messaging import Messaging
 from kafka import KafkaProducer
 
 
--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name='servicex-transformer',
     packages=setuptools.find_packages(),
-    version='0.2',
+    version='0.2.1',
     license='bsd 3 clause',
     description='ServiceX Data Transformer for HEP Data',
     long_description=long_description,
