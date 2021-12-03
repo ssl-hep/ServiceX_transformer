fb56098e5b129bd4f6f68034a067a7132124d218
Suchandra Thapa <suchandra@gmail.com>
2021-12-03 10:13:21 -1000

    Remove typing import for flake8

--- servicex/transformer/tests/test_arrow_writer.py
+++ servicex/transformer/tests/test_arrow_writer.py
@@ -26,7 +26,6 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 import logging
-import typing
 from collections import OrderedDict
 
 from servicex.transformer.arrow_writer import ArrowWriter
