2c0847ad8f6c37e9c1e5fabc813885d56b86725f
Suchandra Thapa <suchandra@gmail.com>
2021-12-09 09:38:00 -1000

    Remove debug code and fix python2 test issues

--- servicex/transformer/arrow_writer.py
+++ servicex/transformer/arrow_writer.py
@@ -25,6 +25,7 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+import sys
 import time
 import logging
 
@@ -82,7 +83,9 @@ class ArrowWriter:
                         key,
                         sink.getvalue())
 
-                    self.avg_cell_size.append(len(sink.getvalue().to_pybytes()) /
+                    # need the float type conversion so that / operation doesn't use integer div
+                    # TODO: remove float call when we drop support for python 2
+                    self.avg_cell_size.append(float(len(sink.getvalue().to_pybytes())) /
                                               len(transformer.attr_name_list) /
                                               batch.num_rows)
                     total_messages += 1
@@ -107,10 +110,9 @@ class ArrowWriter:
         if self.messaging:
             # need the float type conversion so that / operation doesn't use integer div
             # TODO: remove float call when we drop support for python 2
-            print(self.avg_cell_size)
             avg_avg_cell_size = float(sum(self.avg_cell_size)) / len(self.avg_cell_size) \
                 if len(self.avg_cell_size) else 0
             self.logger.info("Wrote {0} events to {1} ".format(total_messages, topic_name) +
-                             "Avg Cell Size = {0} bytes".format(avg_avg_cell_size))
+                             "Avg Cell Size = {0:.15f} bytes".format(avg_avg_cell_size))
 
         self.logger.info("Real time: {0} minutes".format(round((tock - tick) / 60.0, 2)))
